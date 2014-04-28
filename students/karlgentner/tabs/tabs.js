b8225c5d2745ed498430a3f933c125f82900d229
kgentner <gentnerkarl@gmail.com>
2014-04-28 10:50:26 -0700

    tabs.js updated

--- students/karlgentner/tabs/tabs.js
+++ students/karlgentner/tabs/tabs.js
@@ -1,17 +1,23 @@
-//Karl Gentner - CF F2 Javascript - Tabs Assignment 4/23/14
+//Karl Gentner - CF F2 Javascript - Ajax Fetch Assignment 4/24/14
 
-$(function tabs() {
-	//Default State
-	$(".tabContent").hide(); //hide content of all divs with class=tabContent
-	$(".tabContent#home").show(); //show content of div with id=home
+$(function populateTabs() {
+    $.getJSON("http://rs.hankyates.com:3000/content",function(result){
+        $.each(result, function(tabname, content){
+            //append html using fetched data
+            $("nav ul").append("<li><a href=#" + tabname + ">" + tabname +"</a></li>");
+            $("div.tabOutputArea").append("<div id='" + tabname + "' class='tabContent'>" + content +"</div>");
+        });
+             
+        $(".tabContent").not(":first").hide();//hide all tab content except the first tab
+        $("ul.tabs li:first").addClass("selected").show(); // add "selected" class to first tab and show 
 
-	// On Click
-	$("li").click(function() {
-    	//console.log($(this)[0].outerHTML); // to check what "this" is
-    	$("li").removeClass("selected"); //remove "selected" class from all list items
-    	$(this).addClass("selected");  //add "selected" class to selected list item
-    	$(".tabContent").hide(); //hide content of all divs with class=tabContent
-    	var selectedTab = $("a",this).attr("href"); // return href of selected item
-    	$(selectedTab).show(); // show content of selected item
-	});
-});
+        // On Click
+        $("ul.tabs li").click(function() {
+            $("ul.tabs li").removeClass("selected"); //remove "selected" class from all list items
+            $(this).addClass("selected"); //add "selected" class to selected list item
+            $(".tabContent").hide(); //hide content of all divs with class=tabContent
+            $($("a",this).attr("href")).show(); // show content of selected item
+        });
+    });
+ });
+
